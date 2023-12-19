cabb10dfb11eea99b6a46322af7c5ca2659c1991
@coder shruti <www.meshruti@gmail.com>
2023-12-19 19:27:59 +0530

    rest api (ch-4)

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const server = express()
 //start
 //ab hm apis or paths bnaenge
 //body code
+server.use(express.json()); //body parser
 
 
 //middleware
@@ -31,42 +32,63 @@ const server = express()
 })
 */
 
-const auth =(req,res,next)=>{
-    console.log(req.query);
-    if(req.query.password=='20'){next()}
-    else{
-        res.sendStatus(401);
-    }
-}
-server.use(auth);
 
 
 //middleware
 
 
 //API - Endpoint - Route
-server.get('/',(req,res)=>{
-    res.json({type:'GET'})
+//API ROOT, BASE URL : localhost:8080, google.com, then uske aage url like pro sb add honge
+//products
+///CRUD
+//create api/ create post/products
+server.post('/pro',(req,res)=>{
+    console.log(req.body);
+    pro.push(req.body);
+    res.json(req.body)
+    //res.json({type:'POST'})
 })
-server.post('/',(req,res)=>{
-    res.json({type:'POST'})
+//read get / products
+server.get('/pro',(req,res)=>{
+    //res.json({type:'GET'})
+    res.json(pro)
 })
-server.put('/',(req,res)=>{
-    res.json({type:'PUT'})
+
+server.get('/pro/:id',(req,res)=>{
+    //res.json({type:'GET'})
+    const id=+req.params.id;//+ se hmne string se number me change krliya
+    const p=pro.find(p=>p.id==id)
+    res.json(p)
 })
+
+//update put / products/:id
+server.put('/pro/:id',(req,res)=>{
+    const id=+req.params.id;//+ se hmne string se number me change krliya
+    const pindex=pro.find(p=>p.id==id)
+    pro.splice(pindex,1,{...req.body, id:id})
+    //res.json({product:'updated'})
+    res.status(201).json();
+})
+
+
+//delete DELETE /products/:id
 server.delete('/',(req,res)=>{
-    res.json({type:'DELETE'})
+    const id=+req.params.id;//+ se hmne string se number me change krliya
+    const pindex=pro.find(p=>p.id==id)
+    pro.splice(pindex,1)
+    //res.json({product:'updated'})
+    res.status(201).json();
 })
 //inn sbhi ka path (/) same hi hai means, ek hi path pr alag alag method ho skte hai
 
 
-server.get('/',(req,res)=>{
+//server.get('/',(req,res)=>{
     //res.send('hey')
    // res.sendFile('https://images.unsplash.com/photo-1702651598372-f8e777dabe6c?q=80&w=1944&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')
    //res.json(pro);
    //res.sendStatus(404);
    //res.status(201).send('hello');
-})
+//})
 
 
 //body code
